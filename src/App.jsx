ccd95b0b500a054919c395aae8f47f1e5813b108
MalshiUdesika <malshigamage15@gmail.com>
2025-12-26 22:54:33 +0530

    inizialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import OnSaleNow from "./components/onSaleNow"
+import { ProductCard } from "./components/productCard"
+import UserData from "./components/userData"
+
+function App() {
+
+
+  return (
+    <div>
+      <UserData />
+      <OnSaleNow />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/userData.jsx
@@ -0,0 +1,8 @@
+export default function UserData(){
+
+    return(
+        <div>
+        
+        </div>
+    )
+}
